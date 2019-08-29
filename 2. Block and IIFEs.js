e1faf0b8e839af1e64296d628f502cda0cff097b
Naveen Thomas Varghese <naveen.panickal@gmail.com>
2019-08-29 14:24:33 +0530

    feat: blocks and iife notes

---
+++ 2. Block and IIFEs.js
@@ -0,0 +1,25 @@
+/////////////////////////////////
+// Lecture: Blocks and IIFEs
+
+
+// ES6
+{
+    const a = 1;
+    let b = 2;
+    var c = 3;
+}
+
+//console.log(a + b); // error as a and b are only accessible inside the block 
+console.log(c);// output:3 as var is accessible throughout the function
+
+
+// ES5 /*Instead of using IIFEs , we could create a simple block with let and const to ensure data privacy*/
+(function() {
+    var c = 3;
+})();
+
+//console.log(c);
+
+
+
+
