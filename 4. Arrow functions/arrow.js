16445619b74dc3438e4e79af7c7a3ea7b3a778ea
Naveen Thomas Varghese <naveen.panickal@gmail.com>
2019-08-30 16:27:21 +0530

    feat: arrow function notes

---
+++ 4. Arrow functions/arrow.js
@@ -0,0 +1,110 @@
+/////////////////////////////////
+//  Arrow functions
+
+
+const years = [1990, 1965, 1982, 1937];
+
+// ES5
+var ages5 = years.map(function(el) {
+    return 2016 - el;
+});
+console.log(ages5);
+
+
+// ES6
+let ages6 = years.map(el => 2016 - el);
+console.log(ages6);
+
+ages6 = years.map((el, index) => `Age element ${index + 1}: ${2016 - el}.`); /* when we have no zero arguments or more than one arguments to pass , we use paranthesis eg. (el, index) .*/
+console.log(ages6);
+
+ages6 = years.map((el, index) => {
+    const now = new Date().getFullYear();
+    const age = now - el;
+    return `Age element ${index + 1}: ${age}.`
+});
+console.log(ages6);
+
+
+
+
+
+/////////////////////////////////
+//Arrow functions 2
+
+
+// ES5
+var box5 = {
+    color: 'green',
+    position: 1,
+    clickMe: function() {
+       /* only in a method call "this" keyword points to that object , in regular a function call it points to the window object. Thats why in the next step we we are creating a variable which holds the value of 'this' in a method call.*/
+       
+    var self = this; document.querySelector('.green').addEventListener('click', function() {
+            var str = 'This is box number ' + self.position + ' and it is ' + self.color;
+            alert(str);
+        });
+    }
+}
+//box5.clickMe();
+
+
+// ES6
+const box6 = {
+    color: 'green',
+    position: 1,
+    clickMe: function() {
+        document.querySelector('.green').addEventListener('click', () => {
+            var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+            alert(str);
+        });
+    }
+}
+box6.clickMe();
+
+
+const box66 = {
+    color: 'green',
+    position: 1,
+    clickMe: () => {/* as we made the method call an arrow function, as it shares its lexical this keyword with its surroundings, which is the window object.*/
+        document.querySelector('.green').addEventListener('click', () => {
+            var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+            alert(str);
+        });
+    }
+}
+box66.clickMe();
+
+
+function Person(name) {
+    this.name = name;
+}
+
+// ES5
+Person.prototype.myFriends5 = function(friends) {
+    
+    var arr = friends.map(function(el) {
+       return this.name + ' is friends with ' + el; 
+    }.bind(this));/* as 'this' keyword cannot be accesed inside the above function, we bind 'this' variable of the outer function to the inner function*/
+    
+    console.log(arr);
+}
+
+var friends = ['Bob', 'Jane', 'Mark'];
+new Person('John').myFriends5(friends);
+
+
+// ES6
+Person.prototype.myFriends6 = function(friends) {
+
+    var arr = friends.map(el => `${this.name} is friends with ${el}`);
+
+    console.log(arr);
+}
+
+new Person('Mike').myFriends6(friends);
+
+
+
+
+
